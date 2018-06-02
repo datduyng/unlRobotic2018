011f2d9114dd87b04f665a436b3ba004ff38b45c
Dat Nguyen <35666615+datduyng@users.noreply.github.com>
2018-06-02 08:51:54 -0500

    master commit

---
+++ armCalibration/armCalibration.ino
@@ -0,0 +1,15 @@
+///#include "LedCube.h"
+
+// these include is difference in all Operating System.
+#include "C:\Users\datng\OneDrive\pPlayground\arduino\project\unlRobotic\SCC_Driver\SCC_Driver.h"
+void setup() {
+  // put your setup code here, to run once:
+//  Serial.begin(115200);
+//  Serial1.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  commandMode();
+
+}
---
+++ lib2017/note.md
@@ -0,0 +1,10 @@
+### Servo.h
+- Added min and max pulse when call `Servo` Constructor.
+- https://makezine.com/2014/04/23/arduinos-servo-library-angles-microseconds-and-optional-command-parameters/
+
+### meArm 
+- https://github.com/yorkhackspace/meArm
+- 4dof robot. 
+
+### inverse kinematic 
+- http://web.eecs.umich.edu/~ocj/courses/autorob/autorob_10_ik_closedform.pdf
